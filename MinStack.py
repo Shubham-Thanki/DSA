061fc6b0adf6306d675dc7a4878ab43952c50928
Shubham Thanki <shubhamthanky@gmail.com>
2022-06-17 01:08:10 +0530

    stack_easy

---
+++ MinStack.py
@@ -0,0 +1,34 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.minStack:
+            self.minStack.append(val)
+        elif self.minStack[-1] > val:
+            self.minStack.append(val)
+        else:
+            self.minStack.append(self.minStack[-1])
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        # This function will return the minimum element from the given stack at any moment in O(1) time.
+        return self.minStack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+# https://leetcode.com/problems/min-stack/
