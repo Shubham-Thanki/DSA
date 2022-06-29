7d8680af3b17078df1e8b23ba4c29c998e0ac3af
Shubham Thanki <shubhamthanky@gmail.com>
2022-06-29 17:26:27 +0530

    29-06

--- 1046.last-stone-weight-heap.py
+++ 1046.last-stone-weight-heap.py
@@ -12,15 +12,20 @@ class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         for i in range(len(stones)):
             stones[i] *= (-1)
+            # Here we multiply the elements with (-1) so that we can use the minheap data structure, since python doesen't
+            # support max heap
         heapq.heapify(stones)
         while(len(stones) > 1):
             print(stones)
+            # This value will be greater in magnitude and less in value.
             st1 = heapq.heappop(stones)
+            # This value will be lower in magnitude and more in value.
             st2 = heapq.heappop(stones)
             # -st1-(-st2)
             if st1 < st2:
                 diff = st1-st2
                 heapq.heappush(stones, diff)
+        # We append 0, in case the heap might be empty
         stones.append(0)
         return abs(stones[0])
 # @lc code=end
