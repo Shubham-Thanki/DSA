603762eb5d69299824c42626f6f4f0fbbec17e1b
Shubham Thanki <shubhamthanky@gmail.com>
2022-07-14 10:25:44 +0530

    14-07

--- sorting/insertion.py
+++ sorting/insertion.py
@@ -4,12 +4,11 @@ from re import I
 arr = [12, 45, 23, 51, 8]
 n = 5
 for i in range(1, n):
-    k = i
-    # j=i-1
-    for j in range(i-1, -1, -1):
-        if arr[i] < arr[j]:
-            arr[i], arr[j] = arr[j], arr[i]
-            i -= 1
-    i = k
+    current = arr[i]
+    prev = i-1
+    while(arr[prev] > current and prev >= 0):
+        arr[prev+1] = arr[prev]
+        prev -= 1
+    arr[prev+1] = current
 
 print(arr)
